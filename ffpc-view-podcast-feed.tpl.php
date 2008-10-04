428caa4b3707eebf567a6bb3a78ac38cee8d9860
Matt Farina <matt@mattfarina.com>
2008-10-04 17:53:24 +0000

    This is an overhaul of the ffpc module. - Removed the ffpc display type. Instead defaulting to the core feed type. - Moved the sytle plugin class to be an extends of the core rss plugin syste. The core plugin style provides everything we need except the ability to add aditional elements to the feed itself (such as the iTunes specific tags). The new object adds this extra functionality. - Moved the row plugin to be an extends of the default node row rss plugin provided by core and then extending that to add the aditional files and iTunes/podcast specific tags to the items.
    Moving to this setup provides a number of benefits.
    - Less complexity now that the ffpc display has been removed.
    - Uses move of core so core fixes should benefit this module and core settings (e.g., using the teaser or full node text for the description) are now available.

---
+++ ffpc-view-podcast-feed.tpl.php
@@ -0,0 +1,4 @@
+<?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>"; ?>
+<rss version="2.0" xml:base="<?php print $link; ?>"<?php print $namespaces; ?>>
+  <?php print $channel; ?>
+</rss>
