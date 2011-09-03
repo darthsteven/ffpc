114f24ddf073b4170d84049c41172c1b16137fb4
Steven Jones <steven.jones@computerminds.co.uk>
2011-09-03 17:25:00 +0100

    Correct the spacing.

--- ffpc-view-podcast-feed.tpl.php
+++ ffpc-view-podcast-feed.tpl.php
@@ -1,4 +1,5 @@
 <?php print '<?xml'; ?> version="1.0" encoding="utf-8" <?php print '?>'; ?>
-<rss <?php print $namespaces; ?> version="2.0">
+
+<rss<?php print $namespaces; ?> version="2.0">
   <?php print $channel; ?>
 </rss>
